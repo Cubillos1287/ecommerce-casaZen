ba868c49d82675e9fd38423a714c552e4ecabf51
cubillos1287 <cubillos1287@gmail.com>
2026-01-16 12:23:17 -0300

    conflicto-merge

---
+++ Frontend/src/router/AppRouter.jsx
@@ -0,0 +1,21 @@
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "../views/HomePage";
+import Login from "../views/Login";
+import Register from "../views/Register";
+import Profile from "../views/Profile";
+
+
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
