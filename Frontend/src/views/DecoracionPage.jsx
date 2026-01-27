d88e56f599b12b712a919f9f82fa7080a66eac66
Sinai Colina <sinaicolina@Mac.attlocal.net>
2026-01-27 18:13:07 -0500

    favoritos agregado

--- Frontend/src/views/DecoracionPage.jsx
+++ Frontend/src/views/DecoracionPage.jsx
@@ -1,10 +1,32 @@
 import React, { useContext } from "react";
 import ProductCard from "../components/ProductCard";
 import { ProductContext } from "../context/ProductContext";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+
 
 const DecoracionPage = () => {
+    const navigate = useNavigate();
+
     const { products } = useContext(ProductContext);
+    const { token,
+        favoriteIds = new Set(),
+        addFavorite,
+        removeFavorite
+    } = useContext(UserContext);
+
     const decoracionProductos = products.filter((producto) => producto.categoria === "decoracion");
+
+    const handleFavoriteClick = async (productId, isFavorite) => {
+        if (!token) {
+            navigate("/acceso");
+            return;
+        }
+
+        if (isFavorite) await removeFavorite(productId);
+        else await addFavorite(productId);
+    };
+
 
     return (
         <div className="category-grid">
@@ -17,6 +39,8 @@ const DecoracionPage = () => {
                     descripcion={producto.descripcion}
                     variant="horizontal"
                     precio={producto.precio}
+                    isFavorite={favoriteIds.has(String(producto.id))}
+                    onFavoriteClick={handleFavoriteClick}
                 />
             ))}
         </div>
